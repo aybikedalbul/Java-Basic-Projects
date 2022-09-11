aa2f70d2a892eb4f3454cc84c83e6938f5c025c2
Aybike <100150878+aybikedalbul@users.noreply.github.com>
2022-09-11 15:33:46 +0300

    switch-case atm

--- ATM.java
+++ ATM.java
@@ -1,52 +1,66 @@
 import java.util.Scanner;
-public class Atm {
 
-	public static void main(String[] args) {
-		Scanner scan= new Scanner(System.in);
-	    String islemler= "1.işlem:Bakiye Öğrenme\n"
-	    		        +"2.işlem:Para Çekme\n"
-	    		        +"3.işlem:Para Yatırma\n"
-	    		        +"Çıkış için q'ya basın.";
-	    System.out.println("**********************");
-	    System.out.println(islemler);
-	    System.out.println("**********************");
-	     int bakiye=1000;
-	    while (true){
-	    	System.out.println("İşlem seçiniz: ");
-	    	String islem=scan.nextLine();
-	       if(islem.equals("q"))
-	       {
-	    	   System.out.println("Sistemden çıkış yapılıyor...");
-	    	   break;
-	       }
-	       else if (islem.equals("1")) {
-	    	   System.out.println("Bakiyeniz: " +bakiye);
-	    	   
-	    	  
-	       }
-	       else if(islem.equals("2")) {
-	    	   System.out.println("Para çekmek için tutar giriniz: ");
-	    	   int tutar = scan.nextInt();
-	    	   scan.nextLine();
-	    	   if (tutar>1000) {
-	    		   System.out.println("Bakiye yetersiz.Bakiyeniz:"+bakiye);
-	    	   } 
-	    	   else {
-	    		   bakiye-=tutar;
-	    		   System.out.println("Yeni bakiyeniz: "+bakiye); }
-	       }
-	    	   
-	    	else if (islem.equals("3")) {
-	    	  System.out.println("Yatırmak istediğiniz tutarı giriniz:");
-	    	  int tutar=scan.nextInt();
-	    	  scan.nextLine();
-	    	  bakiye += tutar;
-	    	  System.out.println("Yeni bakiyeniz: "+bakiye);
-	      
-	       
-	    	}
+public class ATM {
 
-	    }
-		
-	}
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("****************************");
+        System.out.println("ATM SİSTEMİNE HOŞ GELDİNİZ!");
+        System.out.println("****************************");
+        int bakiye = 2000, parola=12345;
+        String kullanici_adi = "deneme";
+        String islemler = "İşlemler:\n1-Para Çekme\n2-Para Yatırma\n3-Bakiye Sorgulama\n4-Çıkış Yapma";
+
+        System.out.print("Kullanıcı adınızı giriniz: ");
+        String kul_adi = scanner.nextLine();
+
+        System.out.print("Şifrenizi giriniz: ");
+        int sifre = scanner.nextInt();
+
+        if (kul_adi.equals(kullanici_adi) && sifre == parola) {
+
+            System.out.println("Giriş Başarılı!");
+            System.out.println("****************************");
+
+            System.out.println(islemler);
+
+            System.out.print("Bir işlem seçiniz: ");
+            int secim = scanner.nextInt();
+
+            switch (secim) {
+
+                case 1:
+                    System.out.print("Çekmek istediğiniz tutarı giriniz: ");
+                    int tutar = scanner.nextInt();
+
+                    if (tutar > bakiye) {
+                        System.out.println("Mevcut bakiyenizden daha fazla para çekemezsiniz!");
+                        System.out.println("Bakiye: " + bakiye);
+                    } else {
+                        bakiye -= tutar;
+                        System.out.println("Kalan bakiyeniz: " + bakiye);
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Yatırmak istediğiniz tutarı giriniz: ");
+                    int tutar2 = scanner.nextInt();
+                    bakiye += tutar2;
+                    System.out.println("Yeni bakiyeniz: " + bakiye);
+                    break;
+
+                case 3:
+                    System.out.println("Mevcut Bakiyeniz: " + bakiye);
+                    break;
+
+                case 4:
+                    System.out.println("Çıkış Yaptınız!");
+            }
+        }
+        else {
+            System.out.println("Girişiniz hatalı lütfen tekrar deneyiniz!");
+        }
+
+    }
 }
